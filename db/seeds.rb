77ec57d0c3adc8ed6942202aeae1e06c4d2068a1
Benjamin Shyman <bshyman@gmail.com>
2017-09-24 09:32:00 -0500

    populate seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+6.times do 
+	Interface.create(ip:Faker::Internet.ip_v4_address, hostname: Faker::Internet.domain_word, port: Faker::Number.number(4), ddns: Faker::Internet.url)
+end
