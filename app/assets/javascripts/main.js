b201526956ab43a8a68bc521a6ca94e58ea5cb90
Benjamin Shyman <bshyman@gmail.com>
2017-10-06 15:47:21 -0500

    add animation on delete

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,16 +1,23 @@
 $(document).ready(function(){
 		// alert("Here")
 	var intElements = document.getElementsByTagName('tr');
-	intElements.each(function(key, element){
-		console.log('key ' + key)
+// 	console.log(intElements)
+// 	intElements.each(function(key, element){
+// 		console.log('key ' + key)
+// 	})
+// 	intElements.class;
+// 	var url = "/interfaces/"
+// 	$.ajax({
+// 	  url: "/interfaces",
+// 	  context: document.body
+// 	}).done(function() {
+// 		console.log("This - " + this)
+// 	  $( this ).addClass( "animated zoomOutDown" );
+// });
+
+	$('body').on('click', '.delete_row', function(e){
+		e.preventDefault();
+		$(this).parent().addClass('animated zoomOutDown');
+	
 	})
-	intElements.class;
-	var url = "/interfaces/"
-	$.ajax({
-	  url: "/interfaces",
-	  context: document.body
-	}).done(function() {
-		console.log("This - " + this)
-	  $( this ).addClass( "animated fadeInY" );
-});
 });
