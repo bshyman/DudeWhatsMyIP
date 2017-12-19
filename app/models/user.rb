7e28197425fc146af6fe4d5946a7edc4ddf53cf4
Benjamin Shyman <bshyman@gmail.com>
2017-12-18 21:04:04 -0600

    add sessions controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+	def self.find_or_create_from_auth_hash(auth)
+		where(provider: auth.provider, uid: auth.uid).first_or_initialize.tap do |user|
+			user.provider = auth.provider
+			user.id = auth.uid
+			user.email = auth.info.email
+			# user.picture = auth.info.image
+			user.save!
+		end
+	end
 end
